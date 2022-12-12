c4474ce4015e3d88dbcc58472b17bcd78be605e7
wiliami <wiliaup97@gmail.com>
2022-12-12 06:47:28 -0400

    update code

---
+++ assets/js/forms/cadastro.js
@@ -0,0 +1,38 @@
+function setError(index) {
+    campos[index].style.border = '2px solid #e63656';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+
+
+function nameValidate() {
+    if(campos[0].value.length < 3) {
+        setError(0);
+    }
+    else {
+        removeError(0); 
+    }
+}
+
+function emailValidate() {
+    if(!emailRegex.test(campos[1].value)) {
+        setError(1);
+    }
+    else {
+        removeError(1)
+    }
+}
+
+function passwordValidate() {
+    if(campos[2].value.length < 8) {
+        setError(2)
+    }
+    else {
+        removeError(2)
+    }
+}
