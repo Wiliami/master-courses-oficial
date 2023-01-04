7faf6a09d533e4da77f93e561c9d479b5785844e
wiliami <wiliaup97@gmail.com>
2023-01-04 13:54:40 -0400

    update code

---
+++ Model/PessoaController.js
@@ -0,0 +1,20 @@
+class PessoaContoller {
+
+    // atributos e propriedades
+    _inputName
+    _inputIdade
+    _inputPeso
+    _inputAltura
+
+
+    // metodos
+
+
+    // construtor
+    constructor() {
+        this._inputName = document.querySelector('#');
+        this._inputIdade = document.querySelector('#');
+        this._inputPeso = document.querySelector('#');
+        this._inputAltura = document.querySelector('#');
+    }
+}
